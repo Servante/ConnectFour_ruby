6deb6d8e57c712b30e6790d34e536efb61ca3ab3
Wesley <matthew282@msn.com>
2021-12-20 10:46:32 -0700

    various

--- lib/board.rb
+++ lib/board.rb
@@ -1,13 +1,14 @@
 # frozen_string_literal: true
 
-class Board
-	attr_accessor :cells
+require_relative 'cell.rb'
 
 	def reload
-		load 'board.rb'
-		self.show
-	
+		load 'board.rb'	
 	end
+
+
+class Board
+		attr_accessor :cells
 
 	def initialize
 		# @cells = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42]
@@ -17,10 +18,22 @@ class Board
 					    "row_4" => [21,22,23,24,25,26,27],
 					    "row_5" => [28,29,30,31,32,33,34],
 					    "row_6" => [35,36,37,38,39,40,41]}
-
-
-
 	end
+
+	def create_board
+		hash = []
+		counter = -1
+		(6).times do 
+			array = []
+			(7).times do 
+				new_cell = create_cell
+				array << new_cell
+			end
+			hash["row_#{counter}"] = array
+		end
+		return hash
+	end
+
 
 
 	def show
