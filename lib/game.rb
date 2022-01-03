6e09bd90d2e0f20b3dfd90140a72a974a4ec9208
Wesley <matthew282@msn.com>
2022-01-03 13:29:04 -0700

    completed test coverage for ConnectFour

--- lib/game.rb
+++ lib/game.rb
@@ -14,5 +14,13 @@ class Game
 		@board = Board.new
 	end
 
-
+	def player_creation
+		player_number = player1.nil? ? 1 : 2
+		display_new_player(player_number)
+		name = gets.chomp
+		token = player_number == 1 ? "\e[32m\u2620\e[0m" : "\e[35m\u2620\e[0m"
+		player = Player.new(name, token)
+		@current_player = player if @current_player.nil?
+		return player
+	end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -6,6 +6,7 @@ require '../lib/board.rb'
 require '../lib/cell.rb'
 require '../lib/player.rb'
 require '../lib/display.rb'
+require 'pry'
 
 
 
@@ -13,7 +14,16 @@ describe Game do
 
 	subject (:game) {described_class.new}
 
+
 	describe '#player_creation' do 
+
+		before do
+				player1 = "wes"
+				player2 = "bria"
+				allow(game).to receive(:puts)
+				allow(game).to receive(:gets).and_return(player1, player2)
+				# allow(game).to receive(:gets).and_return(player2)
+			end
 
 		context 'when player creation is called and there is not a current_player' do
 
@@ -22,22 +32,33 @@ describe Game do
 				game.player_creation
 			end
 
-			xit 'with a green skull and crossbones as a token' do
+			it 'with a green skull and crossbones as a token' do
+				player = game.player_creation
+				expect(player.token).to eq("\e[32m\u2620\e[0m")
 			end
 
-			xit 'sets player1 as @current_player' do
+			it 'sets player1 as @current_player' do
+				player = game.player_creation
+				expect(game.current_player).to be(player)
 			end
 		end
 
 		context 'when player creation is called for player2' do
 
-			xit 'creates a new player' do 
+			before do
+				player1 = game.player_creation
+				game.instance_variable_set(:@player1, player1)
+				player2 = game.player_creation
+				game.instance_variable_set(:@player2, player2)
 			end
 
-			xit 'with a magenta skull and crossbones' do
+			it 'creates player 2 with magenta skull and crossbones for a token' do
+				expect(game.player2.token).to be("\e[35m\u2620\e[0m")
 			end
 
-			xit 'and leaves player1 as @current_player'
+			it 'and leaves player1 as @current_player' do 
+				expect(game.current_player.name).to eq("wes")
+			end
 		end
 	end
 end
