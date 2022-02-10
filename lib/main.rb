b24facf271ac26a7608ab5d7a42a1345142f2679
Wesley <matthew282@msn.com>
2022-02-10 11:32:39 -0700

    added #start_game and #replay

---
+++ lib/main.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require_relative "game.rb"
+require_relative "display.rb"
+require_relative "board.rb"
+require_relative "player.rb"
+require_relative "cell.rb"
+
+
+def start_game
+	game = Game.new
+	game.play_game
+	replay
+end
+
+def replay
+	puts "\n\nPlay again? Please enter 'y' for yes and 'n' for no"
+	reply = gets.chomp.downcase
+	if reply == "y"
+		start_game
+	else
+		puts "Thanks for playing!"
+	end
+end
+
+
+start_game
